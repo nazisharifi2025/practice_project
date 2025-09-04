efac740ca288aea675fd91a4d509cc8bfd723231
nazi sharifi <banonazisharifi@gmail.com>
2025-09-04 12:46:32 +0430

    some Commponr=ent added

--- src/Componnent/Count.jsx
+++ src/Componnent/Count.jsx
@@ -1,7 +1,19 @@
+import { act, useReducer } from "react"
+
 export default function Count(){
+    const [valio,despath] = useReducer(reduse,{Number : 0 , click : 0});
+    function reduse(valio , action){
+        if(action.type === "inc"){
+          return  {...valio,Number:valio.Number + action.paylod ,click: valio.click + action.paylod};
+        }else if(action.type === "dec"){
+            return  {...valio,Number:valio.Number - action.paylod ,click: valio.click + action.paylod};
+        }
+    }
     return(
         <div>
-            
+            <h1 className="font-bold text-xl border-b border-gray-200 mb-6">the number of {valio.Number} and the total number is {valio.click}</h1>
+            <button className="px-4 py-2 bg-gray-200 shadow-md shadow-gray-400 mx-3" onClick={()=> despath({type : "inc" , paylod : 1})}>increment</button>
+            <button className="px-4 py-2 bg-gray-200 shadow-md shadow-gray-400 mx-3" onClick={()=> despath({type : "dec" , paylod : 1})}>decrement</button>
         </div>
     )
 }
--- src/Componnent/Online.jsx
+++ src/Componnent/Online.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Slider from "./Slider";
 import Visibility from "./Visiality";
+import Count from "./Count";
 
 export default function Online(){
     const [Online,setOnline] = useState(0);
@@ -14,6 +15,7 @@ export default function Online(){
         <div className="h-screen w-full relative flex justify-center items-center">
             <Slider/>
             <Visibility/>
+            {/* <Count/> */}
             <h1 className="shadow-md shadow-black p-5 absolute bottom-1 right-1 font-bold text-xl">شما {Online}ثانیه است که در این صفحه هستید.</h1>
         </div>
     )
